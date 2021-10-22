2e454849187ec9ba2dd1d8eb9b1e1e0f85834417
ElenaRudenkova <elenarudenkova.musemio@gmail.com>
2021-10-22 21:36:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lection1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Oleh";
+            string SName = "Potomakha";
+            int Age = 22;
+            int ageTo = 40;
+            Console.WriteLine($"Меня зовут {Name} {SName}, мой возраст {Age}. До {ageTo} мне осталось {ageTo - Age} ", Name, SName, Age, ageTo);
+            Console.ReadKey();
+        }
+    }
+}
